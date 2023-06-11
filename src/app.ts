da55b398140eb92d0417f9f0176bcfbcf679c422
krapli441 <tlfqj1208@gmail.com>
2023-06-11 10:08:30 +0900

    타입스크립트 환경에서 express 서버를 구동시킨 것을 확인.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import express from "express";
+import path from "path";
 
 const app = express();
+
+const port = 2080;
+
+app.use(express.static(path.join(__dirname, "../")));
+
+// index.html 페이지
+app.get("/", (req, res) => {
+  res.type("text/html");
+  res.sendFile(__dirname + "/../index.html");
+});
+
+// 404 페이지 작성
+app.use((req, res) => {
+  res.type("text/plain");
+  res.status(404);
+  res.send("404 - Not Found");
+});
+
+app.listen(port, () => {
+  console.log(`서버가 ${port} 포트에서 실행중입니다`);
+});
