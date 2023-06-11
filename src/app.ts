f6c1c806916baf08706ab7acf24bbb2d1a592911
krapli441 <tlfqj1208@gmail.com>
2023-06-11 11:01:32 +0900

    폼 양식 생성, body-parser를 통해 데이터가 넘어오는 것을 확인.

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,58 @@
 import express from "express";
 import path from "path";
+import bodyParser from "body-parser";
+import bcrypt from "bcrypt";
+import mysql from "mysql2/promise";
 
 const app = express();
 
 const port = 2080;
 
+const saltRounds: number = 10;
+
 app.use(express.static(path.join(__dirname, "../")));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // index.html 페이지
 app.get("/", (req, res) => {
   res.type("text/html");
   res.sendFile(__dirname + "/../index.html");
+});
+
+app.post("/register", (req, res) => {
+  // form 데이터 받아오기
+  const { name, email, age, id, password, location, phone } = req.body;
+
+  // 잘 제출됐는지 확인
+  console.log("이름:", name);
+  console.log("이메일:", email);
+  console.log("나이:", age);
+  console.log("아이디:", id);
+  console.log("패스워드:", password);
+  console.log("주소:", location);
+  console.log("휴대전화번호:", phone);
+
+  console.log("여기서부터 암호화 시작");
+
+  bcrypt.hash(name, saltRounds, (err, hash) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("암호화된 비밀번호 : " + hash);
+
+    const hashedPassword: string = hash;
+
+    bcrypt.compare(name, hashedPassword, (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("암호 일치 여부 :" + result);
+    });
+  });
+
+  res.send("회원가입이 완료되었습니다.");
 });
 
 // 404 페이지 작성
